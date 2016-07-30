1a43be12ff60d00ff00331103508872311d7860d
king199025 <korol_dima@list.ru>
2016-07-30 10:45:40 +0300

    migrate error

--- console/migrations/m160620_080648_rename_column_ads_fields_value_table.php
+++ console/migrations/m160620_080648_rename_column_ads_fields_value_table.php
@@ -6,7 +6,7 @@ class m160620_080648_rename_column_ads_fields_value_table extends Migration
 {
     public function up()
     {
-        //$this->dropForeignKey('ads_fields_value_ads_fields_fk', 'ads_fields_value');
+        $this->dropForeignKey('ads_fields_value_ads_fields_fk', 'ads_fields_value');
         $this->renameColumn('ads_fields_value', 'ads_fields_id', 'ads_fields_name');
         $this->alterColumn('ads_fields_value', 'ads_fields_name', \yii\db\Schema::TYPE_STRING . '(255) DEFAULT NULL');
     }
