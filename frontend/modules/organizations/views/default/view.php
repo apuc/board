ea30fd3438fa483b88d42e6394f20f97afc7aa2b
king199025 <korol_dima@list.ru>
2017-12-29 16:58:05 +0300

    fix html organization

--- frontend/modules/organizations/views/default/view.php
+++ frontend/modules/organizations/views/default/view.php
@@ -20,20 +20,23 @@ echo $this->render('_menu', ['slug' => $model->slug]);
 
 <section class="header-shop-fon">
     <div class="container">
-        <?php if(!empty($model['header'])):?>
-            <img src="/<?= $model['header']; ?>" alt="">
-        <?php else: ?>
-            <img src="/img/header-shop-fon.jpg" alt="">
-        <?php endif;?>
-        <?php if(!empty($model['logo'])):?>
-            <a rel="nofollow" target="_blank" href="<?= $model['site']; ?>" class="logo-organization-wrap">
-                <img src="/<?= $model['logo']; ?>" alt="">
-            </a>
-        <?php else:?>
-            <span class="logo-organization-wrap">
+        <div class="header-fons-log">
+            <?php if(!empty($model['header'])):?>
+                <img src="/<?= $model['header']; ?>" alt="">
+            <?php else: ?>
+                <img src="/img/header-shop-fon.jpg" alt="">
+            <?php endif;?>
+            <?php if(!empty($model['logo'])):?>
+                <a rel="nofollow" target="_blank" href="<?= $model['site']; ?>" class="logo-organization-wrap">
+                    <img src="/<?= $model['logo']; ?>" alt="">
+                </a>
+            <?php else:?>
+                <span class="logo-organization-wrap">
                 <img src="/img/org-not-logo-min.jpg" alt="">
             </span>
-        <?php endif; ?>
+            <?php endif; ?>
+        </div>
+
     </div>
     <div class="container">
         <div class="about-shop">
