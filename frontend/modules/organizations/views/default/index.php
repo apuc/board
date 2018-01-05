c8dd81c7bde851b9790325f91a5ea8f1caff7bc9
king199025 <korol_dima@list.ru>
2018-01-05 11:32:59 +0300

    fix organizations

--- frontend/modules/organizations/views/default/index.php
+++ frontend/modules/organizations/views/default/index.php
@@ -1,4 +1,5 @@
 <?php
+$this->registerJsFile('/js/jquery-2.1.3.min.js', ['depends' => [\yii\web\JqueryAsset::className()]]);
 echo \frontend\widgets\ShowSeo::widget(
     [
         'title' => 'Организации на RUBON',
@@ -23,25 +24,19 @@ echo \frontend\widgets\ShowSeo::widget(
 </section>
 <?= \frontend\widgets\ShowOrganizationsSearch::widget(); ?>
 
+
+
 <section class="home-content">
     <div class="container">
 
         <div class="home-contents">
 
             <?php
-            //\common\classes\Debug::prn($category);
             $count = 0;
-            $countAllCat = 0;
             $catArr = [];
-            foreach ($category as $item):?>
-
-
-
-                <?php if ($count == 0): ?>
-                    <div class="row">
-                <?php endif; ?>
-
-                <div class="home-content-item" data-id="<?= $item['id']; ?>">
+            $countAllCat = 0;
+            foreach($category as $item): ?>
+                <div class="home-content-item" data-id="<?= $item['id'];?>">
                     <a href="" class="content-item-thumb">
                         <img src="<?= $item['img']; ?>" alt="<?= $item['name']; ?>" title="<?= $item['name']; ?>">
                     </a>
@@ -49,44 +44,35 @@ echo \frontend\widgets\ShowSeo::widget(
                         <a class="text-title" href=""><?= $item['name']; ?></a>
                     </div>
                 </div>
-
                 <?php
                 $catArr[] = $item['id'];
                 $count++;
-                $countAllCat++;
-                ?>
-                <?php if ($count == 5 || $countAllCat == count($category) ):
+                if($count == 2 || $countAllCat == count($category)):
                     $count = 0;
 
-
-                    ?>
-                    </div>
-                    <?php if (!empty($catArr)): ?>
-                    <?php foreach ($catArr as $value): ?>
-                        <div class="text-about" id="button<?= $value; ?>">
-                            <div class="text-about-title">
-                                <a href="<?= \yii\helpers\Url::to(['/organizations/default/all', 'slug' => $category["$value"]['slug']]); ?>"><b>Смотреть
-                                        все организации</b> в <?= $category["$value"]['name'] ?> </a>
+                    if(!empty($catArr)):
+                        foreach($catArr as $value): ?>
+                            <div class="text-about" data-id="<?= $value; ?>">
+                                <div class="text-about-title">
+                                    <a href="<?= \yii\helpers\Url::toRoute(['/organizatsii/' . $category["$value"]['slug']]); ?>"><b>Смотреть все объявления</b> в <?= $category["$value"]['name']?> </a>
+                                </div>
+                                <div class="text-about-links">
+                                    <?php foreach($category["$value"]['childs'] as $childs): ?>
+                                        <a class="text-about-link" href="<?= \yii\helpers\Url::toRoute(['/organizatsii/' . $childs->slug]); ?>"><?= $childs->name; ?></a>
+                                    <?php endforeach; ?>
+                                </div>
                             </div>
-                            <div class="text-about-links">
-                                <?php foreach ($category["$value"]['childs'] as $childs): ?>
-                                    <a class="text-about-link"
-                                       href="<?= \yii\helpers\Url::toRoute(['/organizations/default/all', 'slug' => $childs->slug]); ?>"><?= $childs->name; ?></a>
-                                <?php endforeach; ?>
+                        <?php endforeach;
 
-                            </div>
-                        </div>
-                    <?php endforeach; ?>
-
-                <?php endif; ?>
-
-
-                    <?php $catArr = []; endif; ?>
-            <?php endforeach; ?>
+                    endif;
+                    $catArr = [];
+                endif;
+            endforeach; ?>
 
 
         </div>
     </div>
 </section>
+
 <?= \frontend\modules\organizations\widgets\ShowNewOrg::widget(); ?>
 <?= \frontend\modules\organizations\widgets\ShowTopOrg::widget(); ?>
