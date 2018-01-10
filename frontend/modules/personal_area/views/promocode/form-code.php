243eddb98df9c8b30f13adb6ec0781b4c2f301c0
king199025 <korol_dima@list.ru>
2018-01-10 13:32:07 +0300

    fix html form promocode

--- frontend/modules/personal_area/views/promocode/form-code.php
+++ frontend/modules/personal_area/views/promocode/form-code.php
@@ -25,12 +25,42 @@ $this->params['breadcrumbs'][] = $this->title;
             </ul>
         </div>
         <div class="kabinet-history__right">
-            <h2>Введите промокод</h2>
-            <form action="<?= Url::to(['/personal_area/promocode/add'])?>" method="post">
-                <?= \yii\helpers\Html::hiddenInput('_csrf', Yii::$app->request->csrfToken); ?>
-                <?= \yii\helpers\Html::textInput('code'); ?>
-                <?= \yii\helpers\Html::input('submit', 'Отправить')?>
-            </form>
+            <main class="cabinet-promo">
+                <div class="title">
+                    <h3>Введите промокод</h3>
+                </div>
+
+                <div class="code">
+                    <form method="POST" action="<?= Url::to(['/personal_area/promocode/add'])?>">
+                        <?= \yii\helpers\Html::hiddenInput('_csrf', Yii::$app->request->csrfToken); ?>
+                        <div>
+                            <label for="code">Добавьте код</label>
+                            <?= \yii\helpers\Html::textInput('code', null, ['id' => 'code']); ?>
+                        </div>
+                        <img src="/img/icons/ruble1.png" alt="">
+                        <?= \yii\helpers\Html::input('submit', 'Отправить',null, ['id' => 'submit'])?>
+                        <p>Данный код дает вам возможность поднять свои объявления в ТОП для быстрой и эффективной
+                            продажи</p>
+                    </form>
+
+                </div>
+
+                <!--<div class="use-code">
+                    <h4>Используйте код прямо сейчас:</h4>
+                    <div class="use-code__variants">
+                        <a href="#">Сделать вип</a>
+                        <a href="#">Выделить объявление</a>
+                        <a href="#">Поднять объявление</a>
+                    </div>
+                </div>-->
+            </main>
+
+            <!--<h2>Введите промокод</h2>
+            <form action="<?/*= Url::to(['/personal_area/promocode/add'])*/?>" method="post">
+                <?/*= \yii\helpers\Html::hiddenInput('_csrf', Yii::$app->request->csrfToken); */?>
+                <?/*= \yii\helpers\Html::textInput('code'); */?>
+                <?/*= \yii\helpers\Html::input('submit', 'Отправить')*/?>
+            </form>-->
 
         </div>
     </div>
