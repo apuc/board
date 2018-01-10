eafb57f26a1fd587267ec0f2aad8ce3547087eac
king199025 <korol_dima@list.ru>
2018-01-10 11:07:29 +0300

    fix ads new

--- console/controllers/AdsNewController.php
+++ console/controllers/AdsNewController.php
@@ -19,7 +19,7 @@ class AdsNewController extends Controller
             ->setTo('korol_dima@list.ru')
             ->setFrom(['noreply@rub-on.ru' => 'RubOn'])
             ->setSubject('Новые объявления на сайте')
-            ->attachContent('На сайте есть не опубликованные объявления')
+            ->setTextBody('На сайте есть не опубликованные объявления')
             ->send();
         echo 'success';
     }
