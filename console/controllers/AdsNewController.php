dfd787f7a59db3ab81f9900e8a1a17e87196cebf
king199025 <korol_dima@list.ru>
2018-01-10 11:00:45 +0300

    ads new

---
+++ console/controllers/AdsNewController.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: king
+ * Date: 10.01.18
+ * Time: 10:48
+ */
+
+namespace console\controllers;
+
+use Yii;
+use yii\console\Controller;
+
+class AdsNewController extends Controller
+{
+    public function actionIndex()
+    {
+        Yii::$app->mailer->compose()
+            ->setTo('korol_dima@list.ru')
+            ->setFrom(['noreply@rub-on.ru' => 'RubOn'])
+            ->setSubject('Новые объявления на сайте')
+            ->send();
+        echo 'success';
+    }
+}
