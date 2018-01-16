231c938633c9cd0d5b86c7e7691b61f849dd216f
king199025 <korol_dima@list.ru>
2018-01-16 15:02:11 +0300

    user reg controller

--- frontend/controllers/user/RegUserController.php
+++ frontend/controllers/user/RegUserController.php
@@ -13,6 +13,7 @@ use common\classes\Debug;
 use dektrium\user\controllers\RegistrationController;
 use dektrium\user\models\RegistrationForm;
 use dektrium\user\models\ResendForm;
+use frontend\models\user\UserDec;
 use Yii;
 use yii\web\NotFoundHttpException;
 
@@ -127,6 +128,47 @@ class RegUserController extends RegistrationController
             'title'  => Yii::t('user', 'Account confirmation'),
             'module' => $this->module,
         ]);*/
+    }
+
+    /**
+     * Displays page where user can create new account that will be connected to social account.
+     *
+     * @param string $code
+     *
+     * @return string
+     * @throws NotFoundHttpException
+     */
+    public function actionConnect($code)
+    {
+        $account = $this->finder->findAccount()->byCode($code)->one();
+
+        if ($account === null || $account->getIsConnected()) {
+            throw new NotFoundHttpException();
+        }
+
+        /** @var User $user */
+        $user = \Yii::createObject([
+            'class'    => UserDec::className(),
+            'scenario' => 'connect',
+            'username' => $account->username,
+            'email'    => $account->email,
+        ]);
+
+        $event = $this->getConnectEvent($account, $user);
+
+        $this->trigger(self::EVENT_BEFORE_CONNECT, $event);
+
+        if ($user->load(\Yii::$app->request->post()) && $user->create()) {
+            $account->connect($user);
+            $this->trigger(self::EVENT_AFTER_CONNECT, $event);
+            \Yii::$app->user->login($user, $this->module->rememberFor);
+            return $this->goBack();
+        }
+
+        return $this->render('connect', [
+            'model'   => $user,
+            'account' => $account,
+        ]);
     }
 
 }
