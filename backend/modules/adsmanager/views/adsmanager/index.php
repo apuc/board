202a48683cc714c34063451895cf3fd3b438c5a4
kirill <apuc06@mail.ru>
2018-03-13 15:29:17 +0300

    ads category search

--- backend/modules/adsmanager/views/adsmanager/index.php
+++ backend/modules/adsmanager/views/adsmanager/index.php
@@ -16,7 +16,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
     <!--<p>
-        <?/*= Html::a('Create Adsmanager', ['create'], ['class' => 'btn btn-success']) */?>
+        <? /*= Html::a('Create Adsmanager', ['create'], ['class' => 'btn btn-success']) */ ?>
     </p>-->
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
@@ -28,61 +28,69 @@ $this->params['breadcrumbs'][] = $this->title;
             /*'user_id',*/
             /*'category_id',*/
             [
-                'attribute'=>'pars',
+                'attribute' => 'pars',
                 'label' => 'Откуда',
                 'format' => 'raw',
-                'content' => function($model) {
+                'content' => function ($model) {
                     $html = '';
-                    if($model->pars == 1){
+                    if ($model->pars == 1) {
                         $html = '<span class="rubon">P</span>';
                     }
-                    if($model->pars == 0){
-                        if($model->site_id == 0){
+                    if ($model->pars == 0) {
+                        if ($model->site_id == 0) {
                             $html = '<span class="rubon">R</span>';
-                        }else{
+                        } else {
                             $site = \backend\modules\access_api\models\AccessApi::find()->where(['id' => $model->site_id])->one();
                             $html = "<span>$site->site</span>";
                         }
 
                     }
                     return $html;
-                }
+                },
             ],
             [
-                'attribute'=>'category_id',
+                'attribute' => 'category_id',
                 'label' => 'Категория',
                 'format' => 'text',
-                'content'=>function($model){
+                'filter' => \kartik\select2\Select2::widget([
+                    'data' => \common\classes\AdsCategory::getCategoryTree(0),
+                    'name' => 'AdsmanagerSearch[category_id]',
+                    'options' => [
+                        'placeholder' => 'Выберите категорию',
+                        'value' => $searchModel->category_id
+                    ]
+                ]),
+                'content' => function ($model) {
                     $listcat = \common\classes\AdsCategory::getListCategory($model->category_id, []);
                     $listcat = array_reverse($listcat);
                     array_shift($listcat);
                     $k = 1;
                     $category = '';
-                    foreach($listcat as $val):
-                         $category .= $val;
-                         ($k == count($listcat)) ? '' : $category .= '<span class="separatorListCategory"> | </span>';
-                         $k++;
+                    foreach ($listcat as $val):
+                        $category .= $val;
+                        ($k == count($listcat)) ? '' : $category .= '<span class="separatorListCategory"> | </span>';
+                        $k++;
                     endforeach;
                     return $category;
-                }
+                },
 
             ],
             //'dt_add',
             [
                 'attribute' => 'dt_add',
-                'format' =>  ['date', 'dd.MM.Y'],
-                'options' => ['width' => '100']
+                'format' => ['date', 'dd.MM.Y'],
+                'options' => ['width' => '100'],
             ],
             //'dt_update',
             [
                 'attribute' => 'dt_update',
-                'format' =>  ['date', 'dd.MM.Y'],
-                'options' => ['width' => '100']
+                'format' => ['date', 'dd.MM.Y'],
+                'options' => ['width' => '100'],
             ],
-             'title',
+            'title',
             // 'content:ntext',
             // 'slug',
-             //'status',
+            //'status',
             /*[
                 'attribute' => 'status',
                 'label' => 'Статус',
@@ -98,15 +106,15 @@ $this->params['breadcrumbs'][] = $this->title;
                 'attribute' => 'status',
                 'label' => 'Статус',
                 'format' => 'html',
-                'value'     => function ( $model ) {
+                'value' => function ($model) {
                     $status = \common\models\db\Status::find()->where(['id' => $model->status])->one();
                     return $status->name;
                 },
-                'filter'    => Html::activeDropDownList(
+                'filter' => Html::activeDropDownList(
                     $searchModel,
                     'status',
                     \yii\helpers\ArrayHelper::map(\common\models\db\Status::find()->all(), 'id', 'name'),
-                    [ 'class' => 'form-control', 'prompt' => '' ] ),
+                    ['class' => 'form-control', 'prompt' => '']),
             ],
 
             // 'views',
@@ -116,20 +124,20 @@ $this->params['breadcrumbs'][] = $this->title;
             [
                 'label' => 'Сети',
                 'format' => 'raw',
-                'value' => function($model){
+                'value' => function ($model) {
                     return $this->render('_soc-link', ['slug' => $model->slug, 'model' => $model]);
-                }
+                },
             ],
             [
                 'label' => 'Индексация',
                 'format' => 'raw',
-                'value' => function($model){
+                'value' => function ($model) {
                     return $this->render('_search_system', ['model' => $model]);
-                }
+                },
             ],
             [
                 'class' => 'yii\grid\ActionColumn',
-                'template' =>'{view}',
+                'template' => '{view}',
             ],
         ],
     ]); ?>
@@ -137,9 +145,8 @@ $this->params['breadcrumbs'][] = $this->title;
 <SCRIPT LANGUAGE="JavaScript">
     <!--
     var newWinSearch;
-    function new_window_search(link)
-    {
-        newWinSearch = window.open(link,'newwin','top=15, left=20, menubar=0, toolbar=0, location=0, directories=0, status=0, scrollbars=1, resizable=0, width=400, height=400');
+    function new_window_search(link) {
+        newWinSearch = window.open(link, 'newwin', 'top=15, left=20, menubar=0, toolbar=0, location=0, directories=0, status=0, scrollbars=1, resizable=0, width=400, height=400');
         //newWin.onload = function () {
         //    newWin.document.write("Публикация...");
         //    newWin.close();
--- common/classes/AdsCategory.php
+++ common/classes/AdsCategory.php
@@ -5,9 +5,6 @@
 
 namespace common\classes;
 
-
-
-
 use common\models\db\Category;
 use yii\helpers\ArrayHelper;
 
@@ -17,7 +14,8 @@ class AdsCategory
      * Получить все главные категории
      * @return array|\yii\db\ActiveRecord[]
      */
-    public static function getMainCategory(){
+    public static function getMainCategory()
+    {
         $category = Category::find()->where(['parent_id' => 0])->all();
         return $category;
     }
@@ -27,13 +25,13 @@ class AdsCategory
      * @param $id
      * @return array|bool|\yii\db\ActiveRecord[]
      */
-    public static function getParentCategory($id){
+    public static function getParentCategory($id)
+    {
         $category = Category::find()->where(['parent_id' => $id])->all();
 
-        if(empty($category)){
+        if (empty($category)) {
             return false;
-        }
-        else{
+        } else {
             return $category;
         }
     }
@@ -44,7 +42,8 @@ class AdsCategory
      * @param $key string
      * @return mixed
      */
-    public static function getCategoryInfo($id, $key){
+    public static function getCategoryInfo($id, $key)
+    {
         $info = Category::find()->where(['id' => $id])->one()->$key;
         return $info;
     }
@@ -55,7 +54,8 @@ class AdsCategory
      * @param $key string
      * @return mixed
      */
-    public static function getCategoryInfoSlug($slug, $key){
+    public static function getCategoryInfoSlug($slug, $key)
+    {
         $info = Category::find()->where(['slug' => $slug])->one()->$key;
         return $info;
     }
@@ -63,14 +63,13 @@ class AdsCategory
     /**
      * Получить содержимое информацию по slug категории
      * @param $slug
-
      * @return mixed
      */
-    public static function getCategoryInfoAll($slug){
+    public static function getCategoryInfoAll($slug)
+    {
         $info = Category::find()->where(['slug' => $slug])->one();
         return $info;
     }
-
 
     /**
      * Получить список всех категорий начиная с последней(Только заголовки категорий)
@@ -79,36 +78,35 @@ class AdsCategory
      * @return array
      */
 
-    public static function getListCategory($id,$arr){
+    public static function getListCategory($id, $arr)
+    {
         $category = Category::find()->where(['id' => $id])->one();
         $arr[] = $category->name;
 
-        if($category->parent_id != 0){
+        if ($category->parent_id != 0) {
             $arr = self::getListCategory($category->parent_id, $arr);
-        }
-        else{
+        } else {
             $arr[] = $category->icon;
         }
         //$arrEnd = array_reverse($arr);
         return $arr;
     }
 
-
-
-    public static function getAllCategory(){
+    public static function getAllCategory()
+    {
         $category = Category::find()->all();
         $catArr = [];
 
         foreach ($category as $item) {
             //Debug::prn($item);
-            if($item->parent_id == 0){
+            if ($item->parent_id == 0) {
                 $catArr[$item->id]['id'] = $item->id;
                 $catArr[$item->id]['name'] = $item->name;
                 $catArr[$item->id]['slug'] = $item->slug;
                 $catArr[$item->id]['img'] = $item->icon;
 
                 foreach ($category as $value) {
-                    if($value->parent_id == $item->id){
+                    if ($value->parent_id == $item->id) {
                         $catArr[$item->id]['childs'][] = $value;
                     }
                 }
@@ -122,7 +120,8 @@ class AdsCategory
      * @param $id
      * @return string
      */
-    public static function getNameCategory($id){
+    public static function getNameCategory($id)
+    {
         $category = Category::findOne($id);
         return $category->name;
     }
@@ -132,12 +131,12 @@ class AdsCategory
      * @param $slug
      * @return mixed
      */
-    public static function getIdCategory($slug){
+    public static function getIdCategory($slug)
+    {
         $id = Category::find()->where(['slug' => $slug])->one();
-        if(!empty($id)){
+        if (!empty($id)) {
             return $id->id;
-        }
-        else{
+        } else {
             return null;
         }
 
@@ -147,20 +146,19 @@ class AdsCategory
      * @param $id
      * @return array
      */
-    public static function getParentAllCategory($id){
+    public static function getParentAllCategory($id)
+    {
         $category = Category::find()->where(['parent_id' => $id])->all();
-        if(!empty($category)){
+        if (!empty($category)) {
             $arryResult = [];
             $arryResult = ArrayHelper::getColumn($category, 'id');
             foreach ($category as $item) {
                 $cat = Category::find()->where(['parent_id' => $item->id])->all();
                 $arryResult = array_merge($arryResult, ArrayHelper::getColumn($cat, 'id'));
             }
-        }
-        else{
+        } else {
             $arryResult = $id;
         }
-
 
         return $arryResult;
     }
@@ -171,11 +169,12 @@ class AdsCategory
      * @param $arr
      * @return array
      */
-    public static function getListCategoryAllInfo($id,$arr){
+    public static function getListCategoryAllInfo($id, $arr)
+    {
         $category = Category::find()->where(['id' => $id])->one();
         $arr[] = $category;
 
-        if($category->parent_id != 0){
+        if ($category->parent_id != 0) {
             $arr = self::getListCategoryAllInfo($category->parent_id, $arr);
         }
 
@@ -183,16 +182,16 @@ class AdsCategory
         return $arr;
     }
 
-
     /**
      * Получить родительскую категорию по текущей
      * @return array|null|\yii\db\ActiveRecord
      *
      */
-    public static function getCurrentMainCategory(){
+    public static function getCurrentMainCategory()
+    {
         $request = \Yii::$app->request;
-        if($request->get('slug')){
-            $cat = Category::find()->where( ['id' => self::getIdCategory($request->get('slug'))] )->one();
+        if ($request->get('slug')) {
+            $cat = Category::find()->where(['id' => self::getIdCategory($request->get('slug'))])->one();
             //Debug::prn(self::getIdCategory($request->get('slug')));
             /*if(!empty($cat)){
                 if($cat->parent_id == 0){
@@ -205,13 +204,11 @@ class AdsCategory
 
             return self::getMainCategoryById($cat);
 
-        }
-        else{
+        } else {
             return null;
         }
 
     }
-
 
     /**
      * Получить главную категорию
@@ -219,31 +216,45 @@ class AdsCategory
      * @param $cat
      * @return array|null|\yii\db\ActiveRecord
      */
-    public static function getMainCategoryById($cat){
-        if(!empty($cat)){
-            if($cat->parent_id == 0){
+    public static function getMainCategoryById($cat)
+    {
+        if (!empty($cat)) {
+            if ($cat->parent_id == 0) {
                 return $cat;
-            }
-            else{
+            } else {
                 $cat = Category::find()->where(['id' => $cat->parent_id])->one();
                 return self::getMainCategoryById($cat);
             }
         }
     }
 
-
-    public static function getCurentCategory(){
+    public static function getCurentCategory()
+    {
         $request = \Yii::$app->request;
-        if($request->get('slug')){
-            $cat = Category::find()->where( ['id' => self::getIdCategory($request->get('slug'))] )->one();
+        if ($request->get('slug')) {
+            $cat = Category::find()->where(['id' => self::getIdCategory($request->get('slug'))])->one();
             //Debug::prn(self::getIdCategory($request->get('slug')));
-            if(!empty($cat)){
+            if (!empty($cat)) {
                 return $cat;
             }
-       }
-        else{
+        } else {
             return null;
         }
+    }
+
+    public static function getCategoryTree($id)
+    {
+        $arr = [];
+        $categories = Category::find()->where(['parent_id' => $id])->all();
+        foreach ((array)$categories as $category){
+            if(Category::find()->where(['parent_id' => $category->id])->all()) {
+                $arr[$category->name] = self::getCategoryTree($category->id);
+            }
+            else {
+                $arr[$category->id] = $category->name;
+            }
+        }
+        return $arr;
     }
 
 }
