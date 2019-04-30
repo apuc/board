85347727d46e9d127524006cf0948791f329f115
cooliofv <cooliofv@gmail.com>
2019-04-30 17:47:37 +0300

    cab hrefs

--- frontend/modules/adsmanager/models/Ads.php
+++ frontend/modules/adsmanager/models/Ads.php
@@ -103,7 +103,7 @@ class Ads extends \common\models\db\Ads
             ->leftJoin('ads_img', '`ads_img`.`ads_id` = `ads`.`id`')
             ->leftJoin('geobase_region', '`geobase_region`.`id` = `ads`.`region_id`')
             ->leftJoin('geobase_city', '`geobase_city`.`id` = `ads`.`city_id`')
-            ->where(['!=','status', 1])
+            ->where(['status' => [Ads::STATUS_ACTIVE]])
             ->andWhere(['!=', '`ads`.`id`', 1])
             ->andFilterWhere(['category_id' => $id])
             ->groupBy('`ads`.`id`');
